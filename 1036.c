41ccb5927c16156b661b9a0451af3b2959629302
sirkhancision <69362030+sirkhancision@users.noreply.github.com>
2020-10-11 22:14:37 -0300

    Create 1036.c

---
+++ 1036.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, x1, x2, delta;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    delta = (pow(b, 2) - 4 * a * c);
+    if (a == 0 || delta < 0) {
+        printf("Impossivel calcular\n");
+        return 0;
+    }
+    delta = sqrt(delta);
+    x1 = (- b + delta) / (2 * a);
+    x2 = (- b - delta) / (2 * a);
+
+    printf("R1 = %.5f\n", x1);
+    printf("R2 = %.5f\n", x2);
+    return 0;
+}
