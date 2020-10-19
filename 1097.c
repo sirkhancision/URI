9b889e393fb73050ea7a915639a6ed8eeea863ad
sirkhancision <69362030+sirkhancision@users.noreply.github.com>
2020-10-19 13:24:39 -0300

    Create 1097.c

---
+++ 1097.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	int inum=1, jnum=7, i;
+
+	for (i=1; ; i++) {
+		if (inum == 9 && jnum == 13)
+			break;
+		if (i % 4 == 0) {
+				i = 1;
+				inum += 2;
+				jnum += 4;
+			}
+		if ((i % 4 != 0) && i != 1) jnum--;
+		printf("I=%d J=%d\n", inum, jnum);
+	}
+
+	return 0;
+}
